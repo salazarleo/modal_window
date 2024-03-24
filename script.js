d469019c696136b1945ad2e69dddd5d4592adc6a
salazarleo <leonardomeg123@gmail.com>
2024-03-24 14:11:43 -0300

    alterações finais

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
-const btnOpenModal = document.querySelectorAll('.show-modal') //querySelectorAll - Seleciona todos as classes deifinida com o mesmo nome
+const btnOpenModal = document.querySelectorAll('.show-modal') //querySelectorAll - Seleciona todos as classes definida com o mesmo nome
 
 
 const openModal = function () {//criamos uma variavel na funçao para pode usar ela em varios eventos
@@ -26,3 +26,15 @@ const closeModal = function () { //criamos uma variavel na funçao para pode usa
 
 btnCloseModal.addEventListener('click', closeModal)//aqui selecionamos a variavel que quermos executar o evento, colocamos qual evento queremos 'click' e colocamos a variavel da função para executar o que esta dentro dela 'closeModal'
 overlay.addEventListener('click', closeModal)//aqui selecionamos a variavel que quermos executar o evento, colocamos qual evento queremos 'click' e colocamos a variavel da função para executar o que esta dentro dela 'closeModal'
+
+
+document.addEventListener('keydown', function (e) { //criamos um evento 'keydown' que significa ao pressionar uma tecla este evento é acionado e criamos um parametro dentro da function 'e' para chamar ele e identificar qual tecla foi poressionada
+    console.log(e.key) // .key é uma propiedade que podemos saber qual tecla esta sendo pressionada e estamos identificando o parametro com este elemento 
+
+    if (e.key === 'Escape') { //se a tecla que esta armazenada no parametro 'e' já com apropiedade '.key' for igual a 'Escape' que seria Esc
+        if (!modal.classList.contains('hidden')) {//para ver se a classe hidden está ativa usamos este codigo modal.classlist.contais('hidden') significa se a variavel modal contem a classe hidden ativa entao chama a função closeModal() que é para fechar a janela
+            closeModal()
+        }
+
+    }
+})
